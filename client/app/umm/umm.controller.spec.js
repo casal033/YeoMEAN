85f8c50d206d426e3e75d7e1e61e4a9f0bc069d4
finze008 <mpsmitch@gmail.com>
2014-09-23 11:53:53 -0500

    Something.

--- client/app/umm/umm.controller.spec.js
+++ client/app/umm/umm.controller.spec.js
@@ -5,7 +5,7 @@ describe('Controller: UmmCtrl', function () {
   // load the controller's module
   beforeEach(module('yeoMeanApp'));
 
-  var UmmCtrl, scope, httpBackend, movieList;
+  var UmmCtrl, scope, httpBackend, courseList;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
@@ -14,14 +14,14 @@ describe('Controller: UmmCtrl', function () {
     UmmCtrl = $controller('UmmCtrl', {
       $scope: scope
     });
-    movieList = [];
+    courseList = [];
       //The following httpBackend things will determine what will happen when an http request is called during our tests
     httpBackend.when('POST', '/api/courses').respond(function(method, url, data, headers) {
         courseList.push(JSON.parse(data));
         return [200, {}, {}];
     });
     httpBackend.when('GET', '/api/courses').respond(courseList);
-      //The 1 is for identifying the _id on the movie in the "database"
+      //The 1 is for identifying the _id on the course in the "database"
     httpBackend.when('DELETE', '/api/courses/1').respond(function() {
         courseList.splice(courseList.indexOf({title:"CSCI", grade:"B", credit: 4}),1);
         return [200, {}, {}];
@@ -33,25 +33,26 @@ describe('Controller: UmmCtrl', function () {
   });
 
   describe('testing add and delete from the database', function() {
-      it('should add a new movie "Batman" with a rating of 9 and return it', function() {
+      it('should add a new course "CSCI" with a grade "B" and credit as 4 and return it', function() {
           scope.newCourse = "CSCI";
           scope.newGrade = "B";
           scope.newCredit = 4;
           scope.addCourse();
           //all of the http requests are added into a queue and are all run when the flush() method is called
           httpBackend.flush();
-          expect(movieList[0]).toEqual({name:"Batman", rating:9});
+          expect(courseList[0]).toEqual({title:"CSCI", grade: "B",credit:4});
       });
 
       it('should return empty object', function() {
-          scope.newMovie = "Batman";
-          scope.newRating = 9;
-          scope.addMovie();
-          //The _id is used so the correct movie is deleted from the "database"
-          scope.deleteMovie({name:"Batman", rating:9, _id:1});
+          scope.newCourse = "CSCI";
+          scope.newGrade = "B";
+          scope.newCredit = 4;
+          scope.addCourse();
+          //The _id is used so the correct course is deleted from the "database"
+          scope.deleteCourse({title:"CSCI", grade: "B", credit:4, _id:1});
           httpBackend.flush();
-          expect(movieList.length).toEqual(0);
-          expect(movieList[0]).toEqual(undefined);
+          expect(courseList.length).toEqual(0);
+          expect(courseList[0]).toEqual(undefined);
       });
   });
 });
