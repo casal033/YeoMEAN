e7dc65cc402b55d0cb9473ce044c631b0b25c2da
finze008 <mpsmitch@gmail.com>
2014-09-25 11:20:16 -0500

    We worked on GPA computation functionality.

--- client/app/umm/umm.controller.js
+++ client/app/umm/umm.controller.js
@@ -9,12 +9,58 @@ angular.module('yeoMeanApp')
         $scope.courseList = courseList;
     });
 
+        function ClassCtrl($scope) {
+            $scope.convertGradeToGPA = function (grade) {
+                switch (grade) {
+                    case "A":
+                        return 4;
+                        break;
+                    case "A-":
+                        return 3.7;
+                        break;
+                    case "B+":
+                        return 3.33;
+                        break;
+                    case "B":
+                        return 3;
+                        break;
+                    case "B-":
+                        return 2.7;
+                        break;
+                    case "C+":
+                        return 2.3;
+                        break;
+                    case "C":
+                        return 2;
+                        break;
+                    case "C-":
+                        return 1.7;
+                        break;
+                    case "D+":
+                        return 1.3;
+                        break;
+                    case "D":
+                        return 1;
+                        break;
+                    case "D-":
+                        return 0.7;
+                        break;
+                    case "F":
+                        return 0;
+                        break;
+                    default:
+                        return 0;
+                }
+                ;
+            };
+        };
+
     $scope.addCourse = function() {
         if($scope.newTitle === ''  || $scope.newGrade === '' || $scope.newCredit === '') {
             return;
         }
         $http.post('/api/courses', { title: $scope.newTitle, grade: $scope.newGrade, credit: $scope.newCredit }).success(function(){
-            //Update courseList to have the same data that's in the database on the sever
+            //Update courseList to have the same data that's in the database on the server
             $http.get('/api/courses').success(function(courseList) {
                 $scope.courseList = courseList;
             });
@@ -32,4 +78,15 @@ angular.module('yeoMeanApp')
             });
         });
     };
+
+            $scope.calculateGPA = function () {
+                var gradePoints = 0;
+                var credits = 0;
+                for(i = 0; i < $scope.courseList.length; i++){
+                    gradePoints += $scope.convertGradeToGPA($scope.courseList[i].grade);
+                    credits += $scope.courseList[i].credit;
+                }
+                return gradePoints/credits;
+            }
+
   });
